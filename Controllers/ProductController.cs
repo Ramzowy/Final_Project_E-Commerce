fe2630192a568e3c63bf7f10136ca79ef65f33d2
Rahim <zowangrm@mail.uc.edu>
2026-03-22 10:27:40 -0400

    Initialized the Git repository with the Product Controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project_E_Commerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
