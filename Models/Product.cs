29194729e3fdc89e859a307eb6be0131744074ef
Rahim <zowangrm@mail.uc.edu>
2026-03-22 10:36:00 -0400

    Added a product model

---
+++ Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Project_E_Commerce.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+
+        [MaxLength(100)]
+        public string Brand { get; set; } = "";
+
+        [MaxLength(100)]
+        public string Category { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = "";
+
+        [MaxLength(100)]
+        public string ImageFileName { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
