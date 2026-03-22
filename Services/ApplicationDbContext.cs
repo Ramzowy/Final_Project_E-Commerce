09cef93db03329d48c33d31fb593cc9ce9adacd3
Rahim <zowangrm@mail.uc.edu>
2026-03-22 10:44:33 -0400

    Initialized the ApplicationDbContext for the Database

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Final_Project_E_Commerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Final_Project_E_Commerce.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
